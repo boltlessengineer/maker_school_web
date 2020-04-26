b6961712b112a913cbd0251aafd4fd7447eae2f8
Seongmin0801 <seongmin0801@gmail.com>
2020-04-26 14:59:58 +0900

    add intro animation!!

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from '../Routes/Home';
+import Intro from '../Routes/Home/Intro';
 import About from '../Routes/About';
 import Contact from '../Routes/Contact';
 import Projects from '../Routes/Projects';
@@ -10,6 +11,7 @@ export default () => (
     <Router>
         <Header />
         <Route path="/" exact component={Home} />
+        <Route path="/intro" exact component={Intro} />
         <Route path="/about" exact component={About} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/projects" exact component={Projects} />
