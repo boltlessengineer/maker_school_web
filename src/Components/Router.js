c97dabc9d2fc975ce1fd4c345ef8e79448b210d4
root <root@DESKTOP-MS327L2.localdomain>
2020-05-03 16:56:30 +0900

    solved refresh problem

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { HashRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Header from "./Header";
 import Home from "../Routes/Home";
 import Intro from "../Routes/Home/Intro";
@@ -10,11 +15,13 @@ import Projects from "../Routes/Projects";
 export default () => (
   <Router>
     <Header />
-    <Route path="/" exact component={Home} />
-    <Route path="/intro" component={Intro} />
-    <Route path="/about" exact component={About} />
-    <Route path="/contact" exact component={Members} />
-    <Route path="/projects" exact component={Projects} />
-    <Redirect from="*" to="/" />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/intro" component={Intro} />
+      <Route path="/about" exact component={About} />
+      <Route path="/contact" exact component={Members} />
+      <Route path="/projects" exact component={Projects} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
